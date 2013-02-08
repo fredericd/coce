4b90fd7a5a27b8d4bec02116af47708814e4eae6
Frédéric Demians <f.demians@tamil.fr>
2013-02-08 09:32:43 +0100

    OO refactoring

--- README.md
+++ README.md
@@ -2,19 +2,29 @@
 
 A cover image cache exposing its content as a REST web service.
 
+In various softwares (ILS for example), Book (or other kind of resources)
+cover image is displayed in front of the resource. Those images are fetched
+automatically from providers, such as Google or Amazon. The both propose web
+services to retrieve information on Books from an ID (ISBN for example).
 
-# Installation
+* [Google API](https://developers.google.com/books/docs/dynamic-links)
+* [Amazon Product Advertising
+  API](https://affiliate-program.amazon.com/gp/advertising/api/detail/main.html)
+
+
+
+## Installation
 
 * Install and start redis
 
 * Edit config.json file
 
 * Install node libraries:
-  
-  npm install express redis aws-lib util
+ 
+        npm install express redis aws-lib util
 
-# Start
+## Start
 
-  cd _Coce HOME_
-  node coce.js
+    cd _Coce HOME_
+    node coce.js
 
--- coce.js
+++ coce.js
@@ -7,153 +7,223 @@ var http = require('http');
 var app = express();
 var util = require('util');
 
+var config = eval('('+fs.readFileSync('config.json','utf8')+')');
+var awsProdAdv = aws.createProdAdvClient(config.aws.accessKeyId,
+      config.aws.secretAccessKey, config.aws.associateTag);
+
 var regGb = new RegExp("(zoom=5)", "g");
 
-/*
- * Coce object
+
+
+/**
+ * UrlRepo class
+ *
+ * @class UrlRepo
+ * @module coce
+ * @constructor
+ * @param {Array} ids Array of images ID
+ * @param {Array} providers Array of images providers (gb or aws)
  */
+function UrlRepo(ids, providers) {
+  this.ids = ids;
+  this.providers = providers;
+  this.count = 0;
+  this.countMax = ids.length * providers.length;
 
-var Coce = {
+  /**
+   * URLs found in the cache (or from providers)
+   * @property url
+   * @type Object
+   */
+  this.url = {};
+}
 
-  c: eval('('+fs.readFileSync('config.json','utf8')+')'),
 
-  init: function() {
-    var c = Coce.c.aws;
-    Coce.awsProdAdv = aws.createProdAdvClient(c.accessKeyId,
-      c.secretAccessKey, c.associateTag);
-  },
-
-  GetCoverFromCache: function(req,res) {
-    var ids = req.query.id;
-    if (ids === undefined || ids.length < 8) {
-      res.send("id parameter is missing");
-      return;
-    }
-    ids = ids.split(',');
-    console.log('ids='+ids + ' - length='+ids.length);
-    var providers = req.query.provider;
-    providers = providers == undefined ? Coce.c.providers : providers.split(',');
-    var found = {};
-    var count = 0, countMax = ids.length * providers.length;
-    console.log("countMax = " + countMax);
-    for (var i=0, id; id = ids[i]; i++) {
-      for (var j=0, provider; provider = providers[j]; j++) {
-        (function() {
-          var key = provider + '.' + id;
-          var _id = id;
-          var _provider = provider;
-          console.log('GET ' + key);
-          redis.get(key, function(err, reply) {
-            console.log('REPLY ' + key + ' ' + reply);
-            if ( reply === null ) {
-              // Not in the cache => search
-              redis.setex(key, Coce.c.timeout, '');
-              if (_provider === 'gb') {
-                var opts = {
-                  host: 'books.google.com',
-                  port: 80,
-                  path: "/books?bibkeys=" + _id + "&jscmd=viewapi",
-                };
-                var req = http.get(opts, function(res) {
-                  res.setEncoding('utf8');
-                  var store = '';
-                  res.on('data', function(data) { store += data });
-                  res.on('end', function() {
-                    //console.log(store);
-                    eval(store);
-                    //console.log(_GBSBookInfo);
-                    for (var id in _GBSBookInfo) {
-                      var url = _GBSBookInfo[id].thumbnail_url;
-                      if ( url === undefined ) { continue; }
-                      // get the medium size cover image
-                      url = url.replace(regGb, 'zoom=1');
-                      redis.setex(key, Coce.c.timeout, url);
-                      if (found[_id] === undefined) { found[_id] = {}; }
-                      found[_id][_provider] = url;
-                      console.log('----------------------');
-                      console.log(key +': ' + url);
-                    }
-                    count++;
-                  });
-                });
-              } else if (_provider === 'aws' ) {
-                var options = { 
-                  SearchIndex: 'All',
-                  Keywords: _id,
-                  ResponseGroup: 'Images'
-                };
-                Coce.awsProdAdv.call('ItemSearch', options, function(err, result) {
-                  //console.log(result.Items);
-                  var items = result.Items;
-                  if ( items.TotalResults > 0 ) {
-                    var item = items.Item;
-                    if (item instanceof Array) { item = item[0]; }
-                    //console.log(util.inspect(item, false, null));
-                    var url = item[Coce.c.aws.imageSize];
-                    if (url !== undefined) { // Amazon has a cover image
-                      var url = url.URL;
-                      redis.setex(key, Coce.c.timeout, url);
-                      if (found[_id] === undefined) found[_id] = {};
-                      found[_id][_provider] = url;
-                    }
-                  }
-                  count++;
-                });
-              } else {
-                count++;
-              }
-            } else if ( reply === '' ) {
-                // In the cache, but not found via provider
-                console.log('  => found in redis NO URL');
-                count++;
-            } else {
-              console.log('  => found in redis URL');
-              if (found[_id] === undefined) found[_id] = {};
-              found[_id][_provider] = reply;
-              console.log(found);
-              count++;
-            } 
-          });
-        })();
-      }
-    }
-    console.log("fin boucle => count: " + count);
-    var tickCount = 5;
-    var timer = function() {
-      tickCount--;
-      if ( tickCount > 0 && count !== countMax) {
-        console.log("Attente 1000");
-        setTimeout(timer,1000);
-        return;
-      }
-      console.log( count !== countMax ? 'Pas tout' : 'On a tout' );
-      console.log('count = ' + count);
-      // URL are picked up by provider priority order (request provider parameter)
-      var ret = {};
-      for (var id in found) {
-        for (var j=0, provider; provider = providers[j]; j++) {
-          var url = found[id][provider];
-          if (url !== undefined) { ret[id] = url; break; }
-        }
-      }
-      console.log(found);
-      var callback = req.query.callback;
-      res.send(callback == undefined
-               ? ret
-               : callback + '(' + JSON.stringify(ret) + ')' );
-    };
-    setTimeout(timer, 10);
-  }
+/**
+ * Test if the repo contains all URLs for its IDs.
+ * @method full
+ * @return {Boolean}
+ */
+UrlRepo.prototype.full = function() {
+  return this.count === this.countMax;
 };
 
-Coce.init();
-app.listen(Coce.c.port);
+
+/**
+ * Retrieve an ID from Google Books 
+ * @method gb
+ * @param {String} id The resource ID to request to Google Books
+ */
+UrlRepo.prototype.gb = function(id) {
+  var repo = this;
+  var key = 'gb.' + id;
+  var opts = {
+    host: 'books.google.com',
+    port: 80,
+    path: "/books?bibkeys=" + id + "&jscmd=viewapi",
+  };
+  var req = http.get(opts, function(res) {
+    res.setEncoding('utf8');
+    var store = '';
+    res.on('data', function(data) { store += data });
+    res.on('end', function() {
+      //console.log(store);
+      eval(store);
+      //console.log(_GBSBookInfo);
+      for (var id in _GBSBookInfo) {
+        var url = _GBSBookInfo[id].thumbnail_url;
+        if ( url === undefined ) { continue; }
+        // get the medium size cover image
+        url = url.replace(regGb, 'zoom=1');
+        redis.setex(key, config.timeout, url);
+        if (repo.url[id] === undefined) repo.url[id] = {};
+        repo.url[id]['gb'] = url;
+        console.log('----------------------');
+        console.log(key +': ' + url);
+        //console.log(util.inspect(repo, false, null));
+      }
+      repo.count++;
+    });
+  });
+};
+
+
+/**
+ * Retrieve an ID from Amazon Product Advertising API
+ * @method aws
+ * @param {String} id The resource ID to request to Amazon
+ */
+UrlRepo.prototype.aws = function(id) {
+  var repo = this;
+  var options = { 
+    SearchIndex: 'All',
+    Keywords: id,
+    ResponseGroup: 'Images'
+  };
+  awsProdAdv.call('ItemSearch', options, function(err, result) {
+    //console.log(result.Items);
+    var items = result.Items;
+    if ( items.TotalResults > 0 ) {
+      var item = items.Item;
+      if (item instanceof Array) { item = item[0]; }
+      //console.log(util.inspect(item, false, null));
+      var url = item[config.aws.imageSize];
+      if (url !== undefined) { // Amazon has a cover image
+        var url = url.URL;
+        redis.setex('aws.'+id, config.timeout, url);
+        if (repo.url[id] === undefined) repo.url[id] = {};
+        repo.url[id]['aws'] = url;
+        //console.log('AWS added: ' + key + '=' + url);
+        //console.log(util.inspect(repo, false, null));
+      }
+    }
+    repo.count++;
+  });
+};
+
+
+/**
+ * Retrieve an ID from a provider
+ * @method add
+ * @param {String} id ID
+ * @param {String} provider (aws|gb) to search for
+ */
+UrlRepo.prototype.add = function(id,provider) {
+  var repo = this;
+  var key = provider + '.' + id;
+  console.log('GET ' + key);
+  redis.get(key, function(err, reply) {
+    console.log('GET result: ' + key + ' ' + reply);
+    if ( reply === null ) {
+      // Not in the cache => search
+      redis.setex(key, config.timeout, '');
+      if ( provider == 'gb' || provider == 'aws' )
+        repo[provider](id);
+      else
+        repo.count++;
+    } else if ( reply === '' ) {
+        // In the cache, but not url via provider
+        console.log('  => url in redis NO URL');
+        repo.count++;
+    } else {
+      console.log('  => url in redis URL');
+      //console.log( repo.url);
+      if (repo.url[id] === undefined) repo.url[id] = {};
+      repo.url[id][provider] = reply;
+      repo.count++;
+    } 
+  });
+};
+
+
+/**
+ * Fetch all provided ID from cover image providers
+ * @method fetch
+ */
+UrlRepo.prototype.fetch = function() {
+    for (var i=0; id = this.ids[i]; i++)
+        for (var j=0; provider = this.providers[j]; j++)
+            this.add(id, provider);
+};
+
+
+/**
+ * Wait until URLs providers have finished responding or when too many time 
+ * @method waitFetching
+ * @param tick {int} Tick count for the timeout loop
+ * @param timeout {int} Timeout duration
+ * @param finish {Function} Function to execute when all URLs are fetched or time has
+ * elasped
+ */
+UrlRepo.prototype.waitFetching = function(tick, timeout, finish) {
+    var repo = this;
+    var timer = function() {
+        if (--tick > 0 && !repo.full()) setTimeout(timer, timeout);
+        else                            finish();
+    };
+    setTimeout(timer, 10);
+};
+
+
+
+function GetCoverFromCache(req,res) {
+  var ids = req.query.id;
+  if (ids === undefined || ids.length < 8) {
+    res.send("id parameter is missing");
+    return;
+  }
+  ids = ids.split(',');
+  var providers = req.query.provider;
+  providers = providers == undefined ? config.providers : providers.split(',');
+
+  var repo = new UrlRepo(ids, providers);
+  repo.fetch();
+  console.log("fin boucle => count: " + repo.count);
+  
+  repo.waitFetching(5, 1000, function() {
+    console.log( repo.full() ? 'On a tout' : 'Pas tout' );
+    // URL are picked up by provider priority order (request provider parameter)
+    var ret = {};
+    for (var id in repo.url) {
+      for (var j=0, provider; provider = providers[j]; j++) {
+        var url = repo.url[id][provider];
+        if (url !== undefined) { ret[id] = url; break; }
+      }
+    }
+    var callback = req.query.callback;
+    res.send(callback == undefined
+             ? ret
+             : callback + '(' + JSON.stringify(ret) + ')' );
+  });
+}
+
+app.listen(config.port);
 
 app.get('/', function(req, res) {
   res.send('Welcome to coce');
 });
 
-app.get('/cover', Coce.GetCoverFromCache);
+app.get('/cover', GetCoverFromCache);
 
 
 
