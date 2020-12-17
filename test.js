350a42319c222352947fef9fa9c313501171edd5
Frédéric Demians <f.demians@tamil.fr>
2020-12-17 09:47:26 +0000

    Test

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var fetcher = new coce.CoceFetcher(2000);
 var ids = '275403143X,9780415480635,9780821417492,2847342257,9780563533191'.split(',');
 //var ids = '275403143X,9782212124460'.split(',');
 //var ids = '275403143X'.split(',');
-var providers = 'amazon'.split(',');
+var providers = 'aws,gb,ol'.split(',');
 fetcher.fetch(ids, providers, function(url) {
     console.log(url);
     process.exit(0);
