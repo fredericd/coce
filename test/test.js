9ad6f146fee6ed8a70d9b47a8c1864b96ad7fe52
jaideraf <jaideraf@gmail.com>
2024-08-18 14:33:34 -0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ const ids = '275403143X,9780415480635,9780821417492,2847342257,9780563533191'.sp
 //const ids = '275403143X,9782212124460'.split(',');
 //const ids = '275403143X'.split(',');
 const providers = 'aws,gb,ol'.split(',');
-fetcher.fetch(ids, providers, function (url) {
+fetcher.fetch(ids, providers, function(url) {
     console.log(url);
     process.exit(0);
 });
